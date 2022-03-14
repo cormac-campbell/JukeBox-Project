e9f86ca087ace73c63cac8979c88952a46b8bb52
cormac-campbell <101560632+cormac-campbell@users.noreply.github.com>
2022-03-14 10:42:00 +0000

    Add files via upload

---
+++ ITrack.java
@@ -0,0 +1,11 @@
+package part01;
+
+public interface ITrack 
+{
+	public String getDetails();
+	public String getTitle();
+	public String getArtist();
+	public int getDuration();
+	public String getStyle();
+	public String getEncoding();
+}
